a35540940b5187ea315ff00ff1e843434b01ec72
nenl <ehddndpr@naver.com>
2018-01-01 17:00:19 +0900

    11

---
+++ app/src/main/java/com/example/parks/warm_phone_book/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.parks.warm_phone_book;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    //병신같은 안드로이드
+}
