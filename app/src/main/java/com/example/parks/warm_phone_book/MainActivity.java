02dd994cc9abc4f0afb98dce8ad027dbde8a5c7f
hun <qwsdxc313@gmail.com>
2018-01-02 16:40:50 +0900

    min_member_layout 초기 버전

--- app/src/main/java/com/example/parks/warm_phone_book/MainActivity.java
+++ app/src/main/java/com/example/parks/warm_phone_book/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.parks.warm_phone_book;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private LinearLayout minMemberList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        minMemberList = (LinearLayout)findViewById(R.id.minMemberListId);
     }
-    //병신같은 안드로이드
+
+    private void addMinMemeberLayout() { // add min_member_layout
+        for(int i = 0; i < 10; i++) {
+            LayoutInflater inflater = (LayoutInflater)this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            LinearLayout linearLayoutTemp = (LinearLayout)inflater.inflate(R.layout.min_member_layout, null);
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            layoutParams.setMargins(20, 20, 20,20);
+            linearLayoutTemp.setLayoutParams(layoutParams);
+            minMemberList.addView(linearLayoutTemp, 0);
+        }
+    }
 }
